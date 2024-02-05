08581ef99e923aaa237f795517d83183276aaea5
BALASUBRAMANI619 <balasubramaninatraj04@gmail.com>
2024-02-05 06:34:52 -0500

    created new project

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    city = 'London'  # Replace with the desired city name
+    api_key = 'feaaaf970df86af3e97972e4c09330a2'  # Replace with your OpenWeatherMap API key
+
+    # Make API request to OpenWeatherMap
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(weather_url)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        temperature = weather_data['main']['temp']
+        weather_description = weather_data['weather'][0]['description']
+        return render_template('index.html', city=city, temperature=temperature, weather_description=weather_description)
+    else:
+        return f'Error fetching weather data. Status code: {response.status_code}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
